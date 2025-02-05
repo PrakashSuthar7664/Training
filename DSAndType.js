56f8d5bdbab7a22f10fd5e13f3d797214221c731
PrakashSuthar7664 <PrakashSuthar7664@users.noreply.github.com>
2025-02-05 16:40:16 +0530

    Data structure and types

---
+++ DSAndType.js
@@ -0,0 +1,10 @@
+console.log(parseInt("101" , 2)); //5
+console.log(parseInt("111" , 2)); //7
+
+"1.1" + "1.1"; // '1.11.1'
+(+"1.1") + (+"1.1"); // 2.2
+
+
+//Falsy values
+
+// null ,false , undefined,0 , document.all ,NAN
