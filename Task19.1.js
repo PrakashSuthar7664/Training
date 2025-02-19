0126c1417aeda9b9e9627e4b737bbb06283cbd80
PrakashSuthar7664 <PrakashSuthar7664@users.noreply.github.com>
2025-02-19 11:18:28 +0530

    Fibonacci String

---
+++ Task19.1.js
@@ -0,0 +1,17 @@
+// function fibStr(num, arr) {
+// let a = arr[0] ;
+// let b = arr[1] ;
+//   for (let i = 2; i<num ; i++) {
+//       let temp = b+a;
+//       arr.push(temp) ;
+//       a = b ;
+//       b = temp ;
+//   }
+//   return arr
+// }
+// console.log(fibStr(3, ["j", "h"])) //➞ "j, h, hj"
+// console.log(fibStr(5, ["e", "a"]) )//➞ "e, a, ae, aea, aeaae"
+// console.log(fibStr(6, ["n", "k"])) //➞ "n, k, kn, knk, knkkn, knkknknk"
+
+
+
