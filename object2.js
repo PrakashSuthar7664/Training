34ab5c11fd9d47e55b4bb1eafe034d6bed1b9e1b
PrakashSuthar7664 <PrakashSuthar7664@users.noreply.github.com>
2025-02-20 11:58:11 +0530

    Rearrange the Sentence

---
+++ object2.js
@@ -0,0 +1,22 @@
+function combine(user1Income, user2Income) {
+	let merge = {...user1Income} ; 
+    for( let key in user2Income){
+     merge[key] = (merge[key] || 0 ) + user2Income[key] ;         
+     }
+return merge     
+}
+
+console.log(combine({
+    powerPlant: 70000,
+    rental: 12000
+  } ,{
+    teaching: 40000,
+    rental: 10000
+  }))
+  
+  
+//    becomes ➞ {
+//     powerPlant: 70000,
+//     teaching: 40000,
+//     rental: 22000   // The rental income is added together.
+//   }
