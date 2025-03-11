21f0e8fdbded7bc2a2983d51bc391ca2e8c1fc4e
PrakashSuthar7664 <PrakashSuthar7664@users.noreply.github.com>
2025-03-11 15:36:47 +0530

    testing

--- project-1/app.js
+++ project-1/app.js
@@ -5,50 +5,71 @@ const port = 5001;
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post("/register", (req, res) => {
-  const { name, email } = req.body;
-  if (!name || !email) {
-    res.send("name and email required");
-  }
-
-  const query = "insert into users (name,email) values (?, ?)";
-  db.execute(query, [name, email], (err, result) => {
-    if (err) {
-      res.send("databse error");
-    } else {
-      res.json({ success: true, message: "register successfully" });
+async function checkvalid(email) {
+  const query = "SELECT * FROM users where email = ?";
+  db.execute(query, [email], (err, result) => {
+    if(result.length == 0) {
+        console.log("enter")
+      return true;
+    } 
+    if(result[0].email === email) {
+      return false;
+    } 
+    else{
+        return true ; 
     }
   });
+}
+
+app.post("/register", async(req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name ,email);
+  let check = await checkvalid(email);
+  console.log(check,"'JJJJJJJJJJJJJJ")
+  if (!name || !email || !password) {
+    res.send("name email and password fields required");
+  }
+  if (check) {
+    const query = "insert into users (name,email, password) values (?, ? ,?)";
+    db.execute(query, [name, email, password], (err, result) => {
+      if (err) {
+        res.json({ success: false, message: "Database error" });
+      } else {
+        res.json({ success: true, message: "Register Successfully" });
+      }
+    });
+  } else {
+    res.json({ success: false, message: "Email Already Registered" });
+  }
 });
 
 app.post("/login", (req, res) => {
-  const { name, email } = req.body;
-  if (!name || !email) {
-    res.send("name and email required");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.send("Email and Password fields required");
   }
   const query = `SELECT * FROM users WHERE email = ?`;
   db.execute(query, [email], (err, result) => {
     if (err) {
-      res.json({ error: "Database error" });
+      res.json({ success: false, message: "Database error" });
     }
     if (result.length === 0) {
       res.json({
-        "success ": false,
-        message: "user not find",
+        success: false,
+        message: "Invalid credentials",
       });
     }
     const user = result[0];
-    if (name != user.name) {
+    if (password != user.password) {
       res.json({
-        message: "name not match",
+        success: false,
+        message: "Invalid Password",
       });
     } else {
-      res.json(result);
+      res.json({ success: true, message: "Login Successfully" });
     }
   });
 });
-
-app.post("/logout", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
